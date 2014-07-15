377b0afc2c810e40dfdde18017883190b8597f7d
keithbarrow <keith.barrow@tombola.com>
2014-07-15 22:44:20 +0100

    initial commite

---
+++ ExceptionDecorum/ThrownTypeExtractor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ExceptionDecorum
+{
+	public class ThrownTypeExtractor
+	{
+		SemanticModel SemanticModel { get; set; }
+		ThrowStatementSyntax ThrowStatementSyntax { get; set; }
+
+		public ITypeSymbol GetThrownExceptionTypeSymbol(SemanticModel semanticModel, ThrowStatementSyntax throwStatementSyntax)
+		{
+			SemanticModel = semanticModel;
+			ThrowStatementSyntax = throwStatementSyntax;
+
+			SyntaxNode syntaxNodeToGetTypeOf = ThrowStatementSyntax.Expression;
+
+			if (syntaxNodeToGetTypeOf == null)
+				syntaxNodeToGetTypeOf = GetCatchClaue();
+
+			if (syntaxNodeToGetTypeOf == null)
+				return null; //Not sure this is possible....
+			
+			return semanticModel.GetTypeInfo(syntaxNodeToGetTypeOf).Type;
+		}
+
+		private CatchClauseSyntax GetCatchClaue()
+		{
+			return ThrowStatementSyntax
+						.Ancestors()
+						.OfType<CatchClauseSyntax>()
+						.LastOrDefault();
+			
+		}
+	}
+}
